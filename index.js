d2a0343050024c387d515dbafda7bd6e16aec2ab
DevHeris <badairoinioluwa578@gmail.com>
2024-01-15 19:28:08 +0200

    Displayed a simple HTML form

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+// Route handler
 app.get("/", (req, res) => {
-  res.send("All good so far!");
+  res.send(`
+  <div>
+      <form>
+            <input type="email" placeholder="email">
+            <input type="password" placeholder="password">
+            <input type="password" placeholder="password confirmation">
+            <button>Sign UP</button>
+      </form>
+    </div>
+  `);
 });
 
 app.listen(3000, () => {
