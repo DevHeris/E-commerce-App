3cee58bf1f1833ec79f26f92afbcc4d0be3ffdb2
DevHeris <badairoinioluwa578@gmail.com>
2024-01-15 20:10:59 +0200

    default GET method to POST method

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const app = express();
 app.get("/", (req, res) => {
   res.send(`
   <div>
-      <form>
-            <input type="email" placeholder="email">
-            <input type="password" placeholder="password">
-            <input type="password" placeholder="password confirmation">
-            <button>Sign UP</button>
+      <form method="POST">
+            <input name="email" type="email" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <input name="passwordConfirmation" type="password" placeholder="password confirmation">
+            <button>Sign Up</button>
       </form>
     </div>
   `);
+});
+
+app.post("/", (req, res) => {
+  res.send("Account Created");
 });
 
 app.listen(3000, () => {
